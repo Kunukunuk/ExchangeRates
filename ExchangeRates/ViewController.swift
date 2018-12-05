b5e5a8d3d1fb7d132f9b919f860247c564e8407c
Kun Huang <kkun.huang@gmail.com>
2018-12-04 22:45:33 -0500

    initial commit with testing api results and base picker view

--- ExchangeRates/ViewController.swift
+++ ExchangeRates/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var basePickerView: UIPickerView!
+    @IBOutlet weak var baseLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        basePickerView.delegate = self
+        basePickerView.dataSource = self
+        
+        getAPIData()
     }
 
+    func getAPIData() {
+        
+        let url = URL(string: "https://api.exchangeratesapi.io/latest?base=USD")
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            guard let dataJSON = data else {
+                print("error with \(error?.localizedDescription)")
+                return
+            }
+            
+            let dataDictionary = try! JSONSerialization.jsonObject(with: dataJSON, options: []) as! [String: Any]
+            
+            print(dataDictionary)
+        }
+        task.resume()
+    }
 
 }
 
